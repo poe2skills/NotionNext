716264eb00d37d9918b595405c3a48762d8141d2
poe2skills <liqiaoa@gmail.com>
2024-12-16 11:39:59 +0800

    Update index.js

--- themes/game/index.js
+++ themes/game/index.js
@@ -313,7 +313,7 @@ const LayoutSlug = props => {
             <div className={`w-full md:py-2`}>
               {/* 游戏窗口 */}
               <GameEmbed post={post} siteInfo={siteInfo} />
-              <GameListIndexCombine posts={randomGames}
+              <GameListIndexCombine posts={randomGames}/>
               {/* 资讯 */}
               <div className='game-info  dark:text-white py-2 px-2 md:px-0 mt-14 md:mt-0'>
                 {/* 关联游戏 */}
@@ -337,7 +337,6 @@ const LayoutSlug = props => {
             </div>
           </div>
           {/* 其它游戏列表 */}
-         />
         </div>
       )}
     </>
